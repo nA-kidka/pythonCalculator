e776aa7e79a1b3097ab923cd243a9061e93eee30
nA-kidka <R.WOLFMED@yandex.ru>
2022-10-04 10:22:46 +0300

    calculatorPush

---
+++ main.py
@@ -0,0 +1,27 @@
+import telepot as telepot
+from telepot.loop import MessageLoop
+
+TOKEN = ''
+bot = telepot.Bot(TOKEN)
+
+START_MESSAGE = "Привет! Это бот - калькулятор /nНачни вводить пример вида '2+3'  "
+
+
+def handle(msg):
+    """ Process request like '3+2' """
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    text = msg["text"]
+    try:
+        answer = eval(text)
+    except:
+        answer = "Пример решить не могу"
+    bot.sendMessage(chat_id, "Ответ: {}".format(answer))
+
+
+MessageLoop(bot, handle).run_as_thread()
+
+# Keep the program running.
+while True:
+    n = input('To stop enter "stop":')
+    if n.strip() == 'stop':
+        break
